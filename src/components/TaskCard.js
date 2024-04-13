f4ce5da76fdb513b67a5f2136acaffafeea4b9d4
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-04-13 16:06:13 +0300

    add css modules

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,7 +1,9 @@
+import styles from './TaskCard.module.css';
+
 export const TaskCard = ({task, handleDelete}) => {
   return (
     <li key={task.id} className={task.completed ? "completed" : "incomplete"}>
-      <span style={{border: '2px solid brown'}}>
+      <span className={styles.title} style={{border: '2px solid brown'}}>
         {task.id} - {task.name}
       </span>
       <button onClick={() => handleDelete(task.id)} className="sub">
